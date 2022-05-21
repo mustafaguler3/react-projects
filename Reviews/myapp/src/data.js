5c6f3b225b37a333e2d4a0e331b64c1d61fa9741
mustafaguler3 <mustafa.glr@hotmail.com>
2022-05-21 22:54:08 +0300

    added img into review project

--- Reviews/myapp/src/data.js
+++ Reviews/myapp/src/data.js
@@ -3,7 +3,7 @@ const reviews = [
         id:1,
         name:"mustafa güler",
         job:"software developer",
-        image :"https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg"
+        image :"https://i.ibb.co/25dTJz0/profile-pic-10.png"
     },
     {
         id:2,
