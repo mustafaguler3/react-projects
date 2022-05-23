e3a72787b6a0b68169cebe69ae842529b10f7324
mustafaguler3 <mustafa.glr@hotmail.com>
2022-05-23 08:58:17 +0300

    added slider project

---
+++ Slider/myapp/src/data.js
@@ -0,0 +1,22 @@
+const people = [
+    {
+        id:1,
+        name:"mustafa güler",
+        title:"software developer",
+        image :"https://i.ibb.co/25dTJz0/profile-pic-10.png"
+    },
+    {
+        id:2,
+        name:"hakan sezer",
+        title:"senior software developer",
+        image :"https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg"
+    },
+    {
+        id:3,
+        name:"musa güler",
+        title:"Front-end developer",
+        image :"https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg"
+    }
+]
+
+export default people;
